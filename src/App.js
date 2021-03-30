08f9b248a843762b80acf0e23b18bbeb35a77931
okorkov <alex.okorkov@gmail.com>
2021-03-30 16:48:28 -0700

    added Navbar, Footer and Shopping cart Components, npx pachage library rebuilt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
-
-function App() {
-  return (
-    <div className="App">
-      app
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Footer />
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <footer className="footer text-lg-start fixed-bottom">
+      <div className="text-center p-3" >
+        <p>Shop Easy © {new Date().getFullYear()} by Alex Okarkau</p>
+      </div>
+  </footer>
+  )
+}
+
+export default Footer;
