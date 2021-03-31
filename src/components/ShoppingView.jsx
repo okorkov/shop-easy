783adbed8e116fd621cdd9d16abcfd3d5e8ed1c9
okorkov <alex.okorkov@gmail.com>
2021-03-31 16:39:06 -0700

    added single project element

--- src/components/ShoppingView.jsx
+++ src/components/ShoppingView.jsx
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import ProductCard from './ProductCard';
+import SingleProduct from './SingleProduct';
 
 class ShoppingView extends Component {
 
+  constructor(props) {
+    super(props)
+    this.renderProduct = this.renderProduct.bind(this)
+}
+
   state ={
     dataLoaded: false,
-    productData: []
+    productData: [],
+    productId: null
   }
 
   componentDidMount() {
@@ -18,16 +25,22 @@ class ShoppingView extends Component {
     return this.state.productData.map(product => <ProductCard key={product.id} data={product} renderFunction={this.renderProduct}/>)
   }
 
-  renderProduct (param) {
-    console.log(param)
+  renderProduct (id) {
+    this.setState({
+      ...this.state,
+      productId: id 
+    })
   }
 
   render() {
     return (
       <div className="shopping-view">
-        <Grid container spacing={8} justify="center" alignItems="stretch" style={{paddingTop: '5%'}}>
+        {(this.state.productId) ? 
+          <SingleProduct /> :
+          <Grid container spacing={8} justify="center" alignItems="stretch" style={{paddingTop: '5%'}}>
           {(this.state.dataLoaded) ? this.renderProducts() : <CircularProgress />}
-        </Grid>
+         </Grid> 
+        }
       </div>
     );
   }
