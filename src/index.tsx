7333db88621fae9ef4cfa83cc7fbe20e8349b342
okorkov <alex.okorkov@gmail.com>
2021-06-07 09:46:11 -0700

    redux dev tools now compble with safari

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ const store = createStore(
   rootReducer,
   compose(
       applyMiddleware(thunk),
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+      (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__() || compose
   )       
 
 );
